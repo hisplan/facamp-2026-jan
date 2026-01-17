62bd74873b72ee5dd42ae4aff968f5b6526f03b4
Jaeyoung Chun <jaeyoung.chun@gmail.com>
2026-01-17 15:41:06 +0900

    Fix

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
- docker run --rm -it \
-    --platform linux/amd64 \
-    --name gfr \
-    -v ./workspace:/workspace \
-    -w /workspace \
-    -p 8888:8888 \
-    gfootball:2.10.2-facamp.1
+docker run --rm -it \
+   --platform linux/amd64 \
+   --name gfr \
+   -v ./workspace:/workspace \
+   -w /workspace \
+   -p 8888:8888 \
+   gfootball:2.10.2-facamp.1
